d17f0049565943e1ad45bb770aab32cd720692d3
oy0rzabal <67671626+oy0rzabal@users.noreply.github.com>
2023-10-17 07:52:21 -0600

    comit

--- app/bases/views.py
+++ app/bases/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 # Create your views here.
 
 class Home(generic.TemplateView):
-    template_name = 'base/base.html'
+    template_name = 'bases/home.html'
+    #Haremos que nos redireccione al login
+    login_url='/admin'
